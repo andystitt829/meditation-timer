93c79affb3fcf592a9da2f9b639c00b43083da51
Andy Stitt <andystitt829@gmail.com>
2024-08-27 19:41:31 -0400

    Get rid of test page for UseSound utility.

--- src/views/Tab2Page.vue
+++ src/views/Tab2Page.vue
@@ -11,13 +11,11 @@
           <ion-title size="large">Tab 2</ion-title>
         </ion-toolbar>
       </ion-header>
-    <TestUseSound />
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
 import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import TestUseSound from '@/components/TestUseSound.vue';
 
 </script>
